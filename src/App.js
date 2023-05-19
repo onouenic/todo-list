dd766519506969569ce6b48d7c6b4e2c2de55e05
Filipe Onoue <filpeflom@gmail.com>
2023-05-19 08:33:19 -0300

    feat: :sparkles: implemented useContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { createContext, useState } from 'react';
 import './App.css';
+import TaskList from './components/TaksList';
+
+export const Context = createContext();
 
 function App() {
+
+  const [ tasks, setTasks ] = useState([
+    {
+      id: 1,
+      title: 'Tarefa 1',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Tarefa 2',
+      completed: false,
+    },
+  ]);
+
   return (
     <div className="App">
-      Hello World !
+      <Context.Provider
+        value={{ tasks, setTasks }}
+      >
+        <TaskList />
+      </Context.Provider>
     </div>
   );
 }
