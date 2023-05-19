7018753f33b4ceed6250ae3703a00319aba54c41
Filipe Onoue <filpeflom@gmail.com>
2023-05-19 13:53:30 -0300

    refactor: :recycle: removed Context.Provider

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import { createContext, useState } from 'react';
 import './App.css';
 import TaskList from './components/TasksList';
 
-export const Context = createContext();
-
 function App() {
-
-  const [ tasks, setTasks ] = useState([
-    {
-      id: 1,
-      title: 'Tarefa 1',
-      completed: false,
-    },
-    {
-      id: 2,
-      title: 'Tarefa 2',
-      completed: false,
-    },
-  ]);
 
   return (
     <div className="App">
-      <Context.Provider
-        value={{ tasks, setTasks }}
-      >
-        <TaskList />
-      </Context.Provider>
+      <TaskList />
     </div>
   );
 }
