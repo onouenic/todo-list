d719ee114f613240aaa28a4dce0259f805bb8eb7
Filipe Onoue <filpeflom@gmail.com>
2023-05-19 15:23:47 -0300

    refactor: :recycle: refact to use useReducer instead useState

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,41 +1,42 @@
 import { useRef } from "react";
 
-export default function Task({ task, tasks, setTasks }) {
+export default function Task({ task, tasks, setTasks, dispatch }) {
 
-  const titleRef = useRef();
   const liRef = useRef();
 
   const handleChangeCompleted = (e) => {
     e.preventDefault();
+    dispatch({ type: 'TOGGLE_TASK', payload: task.id });
     if (liRef.current.className.includes('bg-green-600')) {
       liRef.current.className = liRef.current.className.replace('bg-green-600', '');
     } else {
       liRef.current.className += ' bg-green-600';
     }
-    tasks.forEach((t, index) => {
+    /* tasks.forEach((t, index) => {
       if (t.id === task.id) {
         const newTasks = [...tasks];
         newTasks[index].completed = !newTasks[index].completed;
         setTasks(newTasks);
       }
-    });
+    }); */
   }
 
   const handleDelete = (e) => {
     e.preventDefault();
-    tasks.forEach((t, index) => {
+    dispatch({ type: 'REMOVE_TASK', payload: task.id });
+    /* tasks.forEach((t, index) => {
       if (t.id === task.id) {
         const newTasks = [...tasks];
         newTasks.splice(index, 1);
         setTasks(newTasks);
       }
-    })
+    }) */
   }
 
   return (
     <li ref={liRef} className={`flex justify-between gap-4 p-8 items-center border shadow-md rounded-md h-[10%] w-[80%] `}>
       <span>{task.id}: </span>
-      <span ref={titleRef}>{task.title}</span>
+      <span>{task.title}</span>
       <div className="flex gap-4">
         <button
           className="border shadow-md rounded-md w-24 h-10 bg-orange-600 hover:transition-colors hover:bg-orange-500"
