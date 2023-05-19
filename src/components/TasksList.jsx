98bdf1ffee9c805835e86cbe78c7188698dada68
Filipe Onoue <filpeflom@gmail.com>
2023-05-19 15:11:07 -0300

    feat: :sparkles: implemented styles in TaskList component

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Task from "./Task";
+import { ThemeContext } from "../App";
 
 export default function TaskList() {
 
   const [ tasks, setTasks ] = useState([]);
   const [ title, setTitle ] = useState('');
+  const { theme } = useContext(ThemeContext);
 
   const handleChangeTitle = (e) => {
     setTitle(e.target.value);
@@ -23,15 +25,15 @@ export default function TaskList() {
   }
 
   return (
-    <div>
-      <ul>
+    <div className="flex flex-col gap-4 justify-center items-center h-screen border shadow-md w-[50%]">
+      <ul className="flex flex-col gap-4 w-full items-center">
         {tasks && tasks.map((task) => (
           <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
         ))}
       </ul>
-      <form name="new-task">
-        <input type="text" name="title" value={title} onChange={handleChangeTitle} />
-        <button type="submit" onClick={handleSubmit}>Adicionar</button>
+      <form className="flex flex-col justify-center items-center gap-4" name="new-task">
+        <label className="flex gap-6">Adicionar tarefa: <input className={`flex text-center border shadow-md rounded-md ${ theme === "light" ? 'bg-white text-black' : 'bg-black text-white' }`} type="text" name="title" value={title} onChange={handleChangeTitle} /></label>
+        <button className="border shadow-md rounded-md w-24 h-10" type="submit" onClick={handleSubmit}>Adicionar</button>
       </form>
     </div>
   );
